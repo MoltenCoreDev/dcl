72a41e89a0fe2ca02cf7c0d3e87cedbb2c28bb28
MoltenCoreDev <craxell.tv@gmail.com>
2021-05-30 15:45:52 +0200

    help

---
+++ go.mod
@@ -0,0 +1,10 @@
+module h
+
+go 1.16
+
+require (
+	github.com/bwmarrin/discordgo v0.23.2
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	golang.org/x/sys v0.0.0-20201018230417-eeed37f84f13 // indirect
+)
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+  content string
+  guildID string
+)
+
+func main() {
+  scanner := bufio.NewScanner(os.Stdin)
+  token := "ODQ4NTMzNzY4NjE2MTQ5MDAy.YLOAxA.g7vDM3mJuEEdywZlZlYof52p3Hc"
+  dg, err := discordgo.New(token)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  dg.AddHandler(MessageCreate)
+
+  dg.Identify.Intents = discordgo.IntentsAll
+
+  err = dg.Open()
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  defer dg.Close()
+
+  for {
+    fmt.Println("Give me the guild id")
+    scanner.Scan()
+    guildID = scanner.Text()
+    guild, _ := dg.State.Guild(guildID)
+    for i,channel := range guild.Channels {
+      fmt.Printf("%d. %v \n", i, channel.Name)
+    }
+
+    fmt.Println("Give me the channel number")
+    scanner.Scan()
+    channelID := scanner.Text()
+    channelIndex, _ := strconv.Atoi(channelID)
+    channel := guild.Channels[channelIndex]
+    for {
+      fmt.Printf("Give me the message to send in %v ", channel.Name)
+      scanner.Scan()
+      content = scanner.Text()
+      if content == "/exit" {
+        break
+      }
+      dg.ChannelMessageSend(channel.ID, content)
+    }
+  }
+
+}
+
+
+
+func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+  fmt.Printf("\n%v said %v in %v\n", m.Author.Username, m.Content, m.GuildID)
+}
